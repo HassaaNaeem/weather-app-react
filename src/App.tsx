0f4af87950043d843e590822dc201b4b00cded83
HassaaNaeem <hassaan282006@gmail.com>
2026-05-21 22:14:47 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import { getWeather } from "./api";
+import Card from "./components/cards/Card";
+import DailyForecast from "./components/cards/DailyForecast";
+import HourlyForecast from "./components/cards/HourlyForecast";
+
+function App() {
+  const { data } = useQuery({
+    queryKey: ["weather"],
+    queryFn: () => getWeather({ lat: 50, lon: 50 }),
+  });
+
+  return (
+    <div className="flex flex-col gap-8">
+      <Card title="Current Weather">
+        {JSON.stringify(data?.current)?.slice(0, 100)}
+      </Card>
+
+      <HourlyForecast />
+      <DailyForecast />
+    </div>
+  );
+}
+
+export default App;
